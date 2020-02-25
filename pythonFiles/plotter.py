3cc6b7173b12937523776a79115340ece2e247c9
palmtrey <palmtrey779@gmail.com>
2020-02-25 10:35:53 -0500

    Automatic push by safecommit.sh

--- pythonFiles/plotter.py
+++ pythonFiles/plotter.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+test_excel = pd.read_excel(r'ball_repelling_data.xlsx')
+print(test_excel)
+
+
 plt.plot([1,2,3],[5,6,7])
 plt.show()
 
