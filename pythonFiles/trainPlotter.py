f541cb008387edc337056d94b18b256c97ab5ecb
palmtrey <palmtrey779@gmail.com>
2020-03-03 10:59:05 -0500

    Commit by Cam on March 03, 2020 10:59:05

--- pythonFiles/trainPlotter.py
+++ pythonFiles/trainPlotter.py
@@ -4,3 +4,8 @@ import matplotlib.pyplot as plt
 data = pd.read_excel(r'traindata.xlsx', index_col = "12 V")
 
 print(data)
+
+times = data.loc[data['12 V']]
+
+plt.plot([1, 2, 3], [1, 2, 3])
+plt.show()
